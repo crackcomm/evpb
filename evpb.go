916735b2ccb447102d5dfcd55c2f507159958197
crackcomm <crackcomm@gmail.com>
2015-12-23 02:49:51 +0100

    move protobuf code to evpb_proto.go

--- evpb.go
+++ evpb.go
@@ -1,16 +1,7 @@
 package evpb
 
-import (
-	"reflect"
-
-	"github.com/golang/protobuf/proto"
-)
-
 // Consumer - evpb message consumer interface
 type Consumer func([]byte) error
-
-// ProtoConsumer - evpb protobuf message consumer interface
-type ProtoConsumer func(proto.Message) error
 
 // Interface - evpb interface.
 type Interface interface {
@@ -22,26 +13,4 @@ type Interface interface {
 
 	// Stop - Stops consuming and producing.
 	Stop() error
-}
-
-// Send - Sends protobuf message to evpb.
-func Send(evpb Interface, msg proto.Message) error {
-	body, err := proto.Marshal(msg)
-	if err != nil {
-		return err
-	}
-	return evpb.Send(proto.MessageName(msg), body)
-}
-
-// Consume - Registers protobuf message consumer.
-func Consume(evpb Interface, typ proto.Message, consumer ProtoConsumer) error {
-	elem := reflect.TypeOf(typ).Elem()
-	return evpb.Consume(proto.MessageName(typ), func(body []byte) error {
-		msg := reflect.New(elem).Interface().(proto.Message)
-		err := proto.Unmarshal(body, msg)
-		if err != nil {
-			return err
-		}
-		return consumer(msg)
-	})
 }
---
+++ evpb_proto.go
@@ -0,0 +1,32 @@
+package evpb
+
+import (
+	"reflect"
+
+	"github.com/golang/protobuf/proto"
+)
+
+// ProtoConsumer - evpb protobuf message consumer interface
+type ProtoConsumer func(proto.Message) error
+
+// Send - Sends protobuf message to evpb.
+func Send(evpb Interface, msg proto.Message) error {
+	body, err := proto.Marshal(msg)
+	if err != nil {
+		return err
+	}
+	return evpb.Send(proto.MessageName(msg), body)
+}
+
+// Consume - Registers protobuf message consumer.
+func Consume(evpb Interface, typ proto.Message, consumer ProtoConsumer) error {
+	elem := reflect.TypeOf(typ).Elem()
+	return evpb.Consume(proto.MessageName(typ), func(body []byte) error {
+		msg := reflect.New(elem).Interface().(proto.Message)
+		err := proto.Unmarshal(body, msg)
+		if err != nil {
+			return err
+		}
+		return consumer(msg)
+	})
+}
