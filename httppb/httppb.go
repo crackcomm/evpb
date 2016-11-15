e65ac15700af4f83faaae2a06f2524dc4574a09d
crackcomm <crackcomm@gmail.com>
2016-11-15 05:24:58 +0100

    httppb: retry mechanism

--- httppb/httppb.go
+++ httppb/httppb.go
@@ -19,11 +19,13 @@ import (
 // New - Creates new http evpb.Interface.
 func New(opts ...Option) evpb.Interface {
 	q := &queue{
-		basePath:    "/api/v1/httppb",
-		serverMux:   http.NewServeMux(),
-		concurrency: 1,
-		msgQueue:    make(chan *message, 10000),
-		handlers:    make(map[string]evpb.Consumer),
+		basePath:      "/api/v1/httppb",
+		serverMux:     http.NewServeMux(),
+		msgQueue:      make(chan *message, 10000),
+		handlers:      make(map[string]evpb.Consumer),
+		maxRetries:    3,
+		concurrency:   1,
+		retryInterval: 10 * time.Second,
 	}
 	for _, opt := range opts {
 		opt(q)
@@ -36,6 +38,9 @@ type queue struct {
 	basePath    string
 	topicPrefix string
 	concurrency int
+
+	maxRetries    int
+	retryInterval time.Duration
 
 	msgQueue chan *message
 	handlers map[string]evpb.Consumer
@@ -53,19 +58,7 @@ type message struct {
 }
 
 func (q *queue) Send(topic string, body []byte) (err error) {
-	host := q.hostPool.Get()
-	req, err := http.NewRequest("POST", fmt.Sprintf("http://%s%s", host.Host(), q.path(topic)), bytes.NewReader(body))
-	if err != nil {
-		return
-	}
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		host.Mark(err)
-		return
-	}
-	// Close response body
-	resp.Body.Close()
-	return
+	return q.send(topic, body, q.maxRetries)
 }
 
 func (q *queue) Consume(topic string, consume evpb.Consumer) (err error) {
@@ -115,6 +108,29 @@ func (q *queue) Consume(topic string, consume evpb.Consumer) (err error) {
 
 func (q *queue) Stop() (err error) {
 	return q.httpListen.Close()
+}
+
+func (q *queue) send(topic string, body []byte, maxRetries int) (err error) {
+	host := q.hostPool.Get()
+	req, err := http.NewRequest("POST", fmt.Sprintf("http://%s%s", host.Host(), q.path(topic)), bytes.NewReader(body))
+	if err != nil {
+		return
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		maxRetries--
+		host.Mark(err)
+		if maxRetries == 0 {
+			log.Printf("Failed with error %v, no more retries", err)
+			return
+		}
+		log.Printf("Failed with error %v, Retrying %d more times, in %s", err, maxRetries, q.retryInterval)
+		time.Sleep(q.retryInterval)
+		return q.send(topic, body, maxRetries)
+	}
+	// Close response body
+	resp.Body.Close()
+	return
 }
 
 func (q *queue) path(t string) string {
